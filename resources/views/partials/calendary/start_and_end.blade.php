2c7fe7113a293f6a977217b37d4073d02afa476f
itsasy <ale.maldo097@gmail.com>
2020-10-27 23:19:40 -0500

    Modified: Views of preparation [Sorted, List of taster added with semantic.js]

--- resources/views/partials/calendary/start_and_end.blade.php
+++ resources/views/partials/calendary/start_and_end.blade.php
@@ -1,8 +1,8 @@
-<div class="form-group col-4">
+<div class="form-group col-6">
     <label class="justify-content-start col-6" for="fecha_inicio_evaluacion">Fecha inicial</label>
     <input type="date" name="fecha_inicio_evaluacion" id="fecha_inicio_evaluacion" class="form-control col-6" min="0">
 </div>
-<div class="form-group col-4">
+<div class="form-group col-6">
     <label class="justify-content-start col-6" for="fecha_fin_evaluacion">Fecha final</label>
     <input type="date" name="fecha_fin_evaluacion" id="fecha_fin_evaluacion" class="form-control col-6" min="0">
 </div>
