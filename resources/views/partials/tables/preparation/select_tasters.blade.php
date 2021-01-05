2ca14ba5cee8384134590284b6274632b0261d08
itsasy <45985585+itsasy@users.noreply.github.com>
2021-01-05 18:00:57 -0500

    Correct text to Select Tasters

--- resources/views/partials/tables/preparation/select_tasters.blade.php
+++ resources/views/partials/tables/preparation/select_tasters.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group col-12 mr-3 ml-3">
-    <label for="catadores_selected">Seleccionar catadores</label>
+    <label for="catadores_selected">Seleccionar jueces (mínimo 6 jueces):</label>
     <div class="ui three sixteen wide field">
         <input type="hidden" name="camp" id="camp">
         <select class="ui fluid search dropdown center" multiple="" name="catadores_selected[]"
             id="catadores_selected" required="required">
             
-            <option value="" disabled="disabled" selected>Jueces expertos (mínimo 6 jueces)</option>
+            <option value="" disabled="disabled" selected>Jueces expertos</option>
             
             @foreach ($tasters as $taster)
                 <option value="{{$taster->id_usuario}}">{{$taster->nombres . ' ' .$taster->apellidos}}</option>
